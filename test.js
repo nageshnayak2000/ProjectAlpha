cacb9149d252dc97114a6caf42d78bae1575bd55
[nageshnayak2000] <nageshnayak2050@gmail.com>
2020-05-13 13:40:39 +0530

    Problem solved of the gap

--- test.js
+++
@@ -1,5 +0,0 @@
-function send() {
-  setTimeout(function() {
-    window.open("mailto:" + document.getElementById('email').value + "?subject=" + document.getElementById('subject').value + "&body=" + document.getElementById('message').value);
-  }, 320);
-}
